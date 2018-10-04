9932786ce45c859e05c2b63806392468448a3fc3
Local User <localuser@gan-1263-02.main.ad.rit.edu>
2018-10-04 12:28:02 -0400

    adds source and text files

---
+++ dice_mazes.py
@@ -0,0 +1,12 @@
+class DiceMazes:
+
+    def __init__(self, map):
+        self.map = map
+        print("Dice mazes init")
+
+    def roll_dice():
+        print("Roll dice")
+
+if __name__ == "__main__":
+    dice_mazes = DiceMazes("map")
+    dice_mazes.roll_dice()
